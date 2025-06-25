59a3d7a45aa9679c8e0435a708ab70382fff9f2e
EElBard <101231052+EElBard@users.noreply.github.com>
2025-06-25 12:47:26 -0700

    name

--- Web App/src/components/AdversaryFeatureForm.tsx
+++ Web App/src/components/AdversaryFeatureForm.tsx
@@ -6,6 +6,19 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from './ui/input';
+
 const pb = new PocketBase('http://127.0.0.1:8090')
 
 const formSchema = z.object({
@@ -43,8 +56,31 @@ export function AdversaryFeatureForm() {
     })
 
     // 2. Submit Handler
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        const record = await pb.collection('adversary_features').create(values)
+        .then(() => form.reset())
+        .then(() => toast("Adversary Feature successfully created."))
+    }
 
     return (<>
-
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+                <FormField 
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Feature Name</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Venomous Stinger" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            </form>
+        </Form>
     </>)
 }
