b9daf87c7023480485d5a59b815345cb78b1080e
EElBard <101231052+EElBard@users.noreply.github.com>
2025-06-25 12:39:47 -0700

    form schema

--- Web App/src/components/AdversaryFeatureForm.tsx
+++ Web App/src/components/AdversaryFeatureForm.tsx
@@ -2,6 +2,8 @@
 
 import PocketBase from 'pocketbase';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 const pb = new PocketBase('http://127.0.0.1:8090')
@@ -12,22 +14,36 @@ const formSchema = z.object({
         "Action",
         "Reaction",
         "Passive"
-    ]),
+    ]).optional(),
     range: z.enum([
         "Melee",
         "Very Close",
         "Close",
         "Far",
         "Very Far"
-    ]),
+    ]).optional(),
     damage_type: z.enum([
         "Physical",
         "Magical"
-    ]),
+    ]).optional(),
     desc: z.coerce.string()
 })
 
 export function AdversaryFeatureForm() {
+    // 1. Form Definition
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            type: undefined,
+            range: undefined,
+            damage_type: undefined,
+            desc: ""
+        }
+    })
+
+    // 2. Submit Handler
+
     return (<>
 
     </>)
