0f682225d8d6b6d495e391e66530e8e448381992
EElBard <101231052+EElBard@users.noreply.github.com>
2025-06-26 13:37:52 -0700

    more stuff

--- Web App/src/App.tsx
+++ Web App/src/App.tsx
@@ -6,16 +6,20 @@ import { Card } from './components/ui/card'
 import { Toaster } from './components/ui/sonner'
 
 function App() {
-  return (
-    <>
+  return (<>
+  <div className='flex justify-evenly flex-wrap'>
       <Toaster />
       <Card className='max-w-lg mx-auto p-6 mt-5'>
-        {/* <AdversaryForm /> */}
-        {/* <AdversaryFeatureForm /> */}
+        <AdversaryForm />
+      </Card>
+      <Card className='max-w-lg mx-auto p-6 mt-5'>
+        <AdversaryFeatureForm />
+      </Card>
+      <Card className='max-w-lg mx-auto p-6 mt-5'>
         <DemoPage />
       </Card>
-    </>
-  )
+    </div>
+  </>)
 }
 
 export default App
